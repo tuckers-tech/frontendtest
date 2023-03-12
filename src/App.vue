2efd45666adc3b37f4c5dc7a524d1cea12e0e069
Tucker Hawkinson <hawkinson.tucker@gmail.com>
2023-03-12 09:57:44 -0400

    Add vue via cli

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import ChessBoard from './components/ChessBoard.vue'
+import Sidebar from './components/Sidebar.vue'
+</script>
+
+<template>
+  <main>
+    <ChessBoard />
+    <Sidebar />
+  </main>
+</template>
+
+<style scoped>
+</style>
